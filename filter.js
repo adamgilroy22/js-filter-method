fe7fa3272c4672fd09c5ae04fe536322e7a139e4
Adam Gilroy <adamgilroy22@gmail.com>
2022-11-28 21:57:28 +0000

    Initial commit

--- filter.js
+++ filter.js
@@ -21,6 +21,12 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21); // Returns people who are 21 and over
+console.log(oldEnough); // Prints [ { name: 'Michael', age: 23 } ]
+
+const paul = people.filter(p => p.name === "Paul"); // Filter out just Paul
+console.log(paul); // Prints [ { name: 'Paul', age: 18 } ]
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +61,17 @@ const students = [
     ]
   },
 ];
+
+// Not best way to do it
+const candidates = students.filter(student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5); // Returns an array with only skills which the student has 5 or more years experience
+  return strongSkills.length > 0;
+});
+console.log(candidates); // Prints object items for Jason and Mark only
+
+// Better way
+const has5yearsExp = skill => skill.yrsExperience >= 5; // Function to determine skills with 5 or more years experience
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0; // Returns an array with only skills which the student has 5 or more years experience using the has5yearsExp function
+
+const candidates2 = students.filter(hasStrongSkills); // Filters students by calling hasStrongSkills as the .filter() parameter
+console.log(candidates2); // Prints object items for Jason and Mark only
